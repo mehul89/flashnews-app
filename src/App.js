36bceb1a1d14117c9024e8004432b32f6ac1bb2f
mehul89 <mehulchapaneri8988@gmail.com>
2023-07-23 14:29:29 +0530

    flashnews-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.style.backgroundColor = '#110f16';
+    } else {
+      document.body.style.backgroundColor = 'aliceblue';
+    }
+  }, [darkMode]);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar handleDarkModeToggle={handleDarkModeToggle} darkMode={darkMode} />
+        <Routes>
+          <Route exact path="/" element={<News pageSize={5} country="in" key="general" category="general" darkMode={darkMode} />} />
+          <Route exact path="/business" element={<News pageSize={5} country="in" key="business" category="business" darkMode={darkMode} />} />
+          <Route exact path="/entertainment" element={<News pageSize={5} country="in" key="entertainment" category="entertainment" darkMode={darkMode} />} />
+          <Route exact path="/health" element={<News pageSize={5} country="in" key="health" category="health" darkMode={darkMode} />} />
+          <Route exact path="/science" element={<News pageSize={5} country="in" key="science" category="science" darkMode={darkMode} />} />
+          <Route exact path="/sports" element={<News pageSize={5} country="in" key="sports" category="sports" darkMode={darkMode} />} />
+          <Route exact path="/technology" element={<News pageSize={5} country="in" key="technology" category="technology" darkMode={darkMode} />} />
+        </Routes>
+      </Router>
+      <Footer darkMode={darkMode} />
+    </>
   );
 }
 
---
+++ src/components/News.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import Newsitem from './Newsitem';
+import '../cardstyle.css';
+import { BarLoader } from 'react-spinners';
+import PropTypes from 'prop-types'
+
+class News extends Component {
+
+    static defaultProps = {
+        country: 'in',
+        category: 'general',
+        pageSize: 5
+    };
+
+    static propTypes = {
+        country: PropTypes.string,
+        category: PropTypes.string,
+        pageSize: PropTypes.number,
+    };
+
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+            currentPage: 1,
+            totalPages: 1,
+            loading: true,
+        };
+        document.title= ` ${this.props.category.toUpperCase()} - Flash news`;
+    }
+
+    async componentDidMount() {
+        this.fetchData();
+    }
+
+    async componentDidUpdate(prevProps, prevState) {
+        if (prevState.currentPage !== this.state.currentPage) {
+            this.fetchData();
+        }
+    }
+
+    async fetchData() {
+        try {
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8a4289f64cd343628e5f59a90c6a4263&page=${this.state.currentPage}&pagesize=${this.props.pageSize}`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: parsedData.articles,
+                totalPages: Math.ceil(parsedData.totalResults / 10),
+                loading: false,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    handleNextPage = () => {
+        this.setState(
+            (prevState) => ({ currentPage: prevState.currentPage + 1, loading: true }),
+            () => this.fetchData()
+        );
+    };
+
+    handlePreviousPage = () => {
+        this.setState(
+            (prevState) => ({ currentPage: prevState.currentPage - 1, loading: true }),
+            () => this.fetchData()
+        );
+    }
+
+    render() {
+        const { darkMode } = this.props;
+        const { loading, articles } = this.state;
+
+        return (
+            <>
+                {loading && (
+                    <BarLoader
+                        color="#0076bd"
+                        height={5}
+                        speedMultiplier={0.4}
+                        width={5000}
+                    />
+                )}
+                <div className="container py-5">
+                    <h1
+                        className={` text-center heading ${darkMode ? 'text-white' : 'text-black'}`}
+                        style={{ margin: '50px'}}
+                        id="pageHeaderTitle"
+                    >
+                        Flash news - top {this.props.category} Headlines
+                    </h1>
+                    {!loading &&
+                        articles &&
+                        articles.map((element) => (
+                            <div key={element.url}>
+                                <Newsitem
+                                    title={element.title}
+                                    description={element.description}
+                                    publishedAt={element.publishedAt}
+                                    imgurl={element.urlToImage}
+                                    url={element.url}
+                                    darkMode={darkMode}
+                                />
+                            </div>
+                        ))}
+
+                    <div className="d-flex justify-content-between py-5">
+                        <button
+                            className="btn btn-primary"
+                            disabled={this.state.currentPage === 1}
+                            onClick={this.handlePreviousPage}
+                        >
+                            &laquo; Previous
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            disabled={this.state.currentPage === this.state.totalPages}
+                            onClick={this.handleNextPage}
+                        >
+                            Next &raquo;
+                        </button>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default News;
